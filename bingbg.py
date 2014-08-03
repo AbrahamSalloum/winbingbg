0686dc3d78debc3e7c7a5d64178f72724f2c3790
pygar <pygar@debian>
2014-08-03 17:54:02 +1000

    high res

--- bingbg.py
+++ bingbg.py
@@ -1,7 +1,8 @@
 import ctypes
 import re
 import urllib
-import os 
+import os
+import string
 SPI_SETDESKWALLPAPER = 0x14
 SPIF_UPDATEINIFILE = 0x1
 SPIF_SENDWININICHANGE = 0x2
@@ -12,10 +13,12 @@ for line in index:
     imgmatch = re.search(r'\/az\/hprichbg\/[\w\d\/\_\-]*.jpg', line)
     if imgmatch:
         break
-filename = re.search(r'[A-Z0-9a-z\-\_]*.jpg',imgmatch.group(0))
+filenameReg = re.search(r'[A-Z0-9a-z\-\_]*.jpg',imgmatch.group(0))
+filepath = string.replace(imgmatch.group(0), "1366x768", "1920x1080", 1)
+filename = string.replace(filenameReg.group(0), "1366x768", "1920x1080", 1)
+if os.path.isfile(filename) is not True:
+    bingindex.retrieve("http://www.bing.com/"+filepath,filename)
 
-if os.path.isfile(filename.group(0)) is not True:
-    bingindex.retrieve("http://www.bing.com/"+imgmatch.group(0),filename.group(0))
-
-imgpath = os.getcwd()+"\\" +  filename.group(0)
+imgpath = os.getcwd()+"\\" +  filename
+print imgpath
 ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, imgpath, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE)
