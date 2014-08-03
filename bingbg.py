0f38d69b026e7a6f30fcc76c37318e1e0e1ef0cc
pygar <pygar@debian>
2014-08-03 17:16:30 +1000

    now checks if file exists

--- bingbg.py
+++ bingbg.py
@@ -1,13 +1,10 @@
 import ctypes
 import re
 import urllib
-import os
-import time
-filename = str(int(time.time()))+".jpg"; 
+import os 
 SPI_SETDESKWALLPAPER = 0x14
 SPIF_UPDATEINIFILE = 0x1
 SPIF_SENDWININICHANGE = 0x2
-
 bingindex = urllib.URLopener()
 bingindex.retrieve("http://www.bing.com/", "index")
 index =open('index','r')
@@ -15,7 +12,10 @@ for line in index:
     imgmatch = re.search(r'\/az\/hprichbg\/[\w\d\/\_\-]*.jpg', line)
     if imgmatch:
         break
+filename = re.search(r'[A-Z0-9a-z\-\_]*.jpg',imgmatch.group(0))
 
-bingindex.retrieve("http://www.bing.com/"+imgmatch.group(0),filename)
-imgpath = os.getcwd()+"\\" +  filename 
-print ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, imgpath, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE)
+if os.path.isfile(filename.group(0)) is not True:
+    bingindex.retrieve("http://www.bing.com/"+imgmatch.group(0),filename.group(0))
+
+imgpath = os.getcwd()+"\\" +  filename.group(0)
+ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, imgpath, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE)
