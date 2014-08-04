4aa2c3d9d36e0c03285a57a1b6f4ece1f65f6086
pygar <pygar@debian>
2014-08-04 10:31:50 +1000

    win7 support

--- bingbg.py
+++ bingbg.py
@@ -1,8 +1,11 @@
+ # Abraham Salloum - scrapes Bing image of the day, makes bg
 import ctypes
 import re
 import urllib
 import os
 import string
+from PIL import Image
+
 SPI_SETDESKWALLPAPER = 0x14
 SPIF_UPDATEINIFILE = 0x1
 SPIF_SENDWININICHANGE = 0x2
@@ -19,6 +22,7 @@ filename = string.replace(filenameReg.group(0), "1366x768", "1920x1080", 1)
 if os.path.isfile(filename) is not True:
     bingindex.retrieve("http://www.bing.com/"+filepath,filename)
 
-imgpath = os.getcwd()+"\\" +  filename
-print imgpath
-ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, imgpath, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE)
+imgpath = os.getcwd()+"\\" + filename
+im = Image.open(imgpath)
+bmpimg = im.save(imgpath, "bmp")
+ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, imgpath+".bmp", SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE)
