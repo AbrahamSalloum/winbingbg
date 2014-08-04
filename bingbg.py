d07b6ed4d42a35eee55a88c82bbbcade37502f38
pygar <pygar@debian>
2014-08-04 10:51:30 +1000

    a little neater

--- bingbg.py
+++ bingbg.py
@@ -16,14 +16,16 @@ for line in index:
     imgmatch = re.search(r'\/az\/hprichbg\/[\w\d\/\_\-]*.jpg', line)
     if imgmatch:
         break
+    
 filenameReg = re.search(r'[A-Z0-9a-z\-\_]*.jpg',imgmatch.group(0))
 filepath = string.replace(imgmatch.group(0), "1366x768", "1920x1080", 1)
 filename = string.replace(filenameReg.group(0), "1366x768", "1920x1080", 1)
+filename = filename + ".bmp" 
 if os.path.isfile(filename) is not True:
     bingindex.retrieve("http://www.bing.com/"+filepath,filename)
 
-imgpath = os.getcwd()+"\\" + filename
+imgpath = os.getcwd()+ "\\" + filename
 im = Image.open(imgpath)
 bmpimg = im.save(imgpath, "bmp")
-ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, imgpath+".bmp", SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE)
+ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, imgpath, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE)
 
